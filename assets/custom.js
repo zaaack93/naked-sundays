2ba03906a40f40928d391e47eb7e8def80552250
bouzangad zakaria <bouzangadzakaria@gmail.com>
2023-03-30 17:07:56 +0000

    Revert "Homepage: Tab Collections"
    This reverts commit 0c2498cf9a4ecac3d55cbe34aa4cf1126f15400b.

--- assets/custom.js
+++ assets/custom.js
@@ -1,30 +1,22 @@
 /*
- * Broadcast Theme
- *
- * Use this file to add custom Javascript to Broadcast.  Keeping your custom
- * Javascript in this fill will make it easier to update Broadcast. In order
- * to use this file you will need to open layout/theme.liquid and uncomment
- * the custom.js script import line near the bottom of the file.
- */
+* Broadcast Theme
+*
+* Use this file to add custom Javascript to Broadcast.  Keeping your custom
+* Javascript in this fill will make it easier to update Broadcast. In order
+* to use this file you will need to open layout/theme.liquid and uncomment
+* the custom.js script import line near the bottom of the file.
+*/
 
-(function () {
+
+(function() {
   // Add custom code below this line
-  let count = 0;
-  let intervalId = setInterval(function () {
-    if (document.querySelector(".stamped-badge") !== null) {
-      window.dispatchEvent(new Event("resize"));
-      clearInterval(intervalId);
-    } else {
-      // Increment the count
-      count++;
 
-      // If the count reaches 5, stop the interval
-      if (count === 5) {
-        clearInterval(intervalId);
-      }
-    }
-  }, 1000);
 
-  // ^^ Keep your scripts inside this IIFE function call to
+  
+
+
+
+
+  // ^^ Keep your scripts inside this IIFE function call to 
   // avoid leaking your variables into the global scope.
 })();
